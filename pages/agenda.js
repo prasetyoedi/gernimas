64ca4db04b504daccf3da7e61585e1ebb4a14ebd
Aisa Selvira <aisaselvira2020@gmail.com>
2024-07-10 20:43:55 +0700

    route

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Footer from "@/components/Footer";
-import Link from "next/link";
+import Footer from "../components/Footer";
 
 const Agenda = () => {
     return (
---
+++ pages/riwayat-periksa.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
+import Link from "next/link";
+
+const RiwayatPeriksa = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+};
+
+export default RiwayatPeriksa;
